31c5634357b25529d74377182960bdb28b1a3895
Maryna Kipenko <maryna.i.kipenko@gmail.com>
2021-03-23 18:52:30 +0200

    1st task is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Profile from "./components/Profile/Profile";
 import "./components/Profile/Profile.css";
+import person from "./components/Profile/person.json";
 
 function App() {
-  return <div className="App">
-    <Profile />
-  </div>
-  
+  return (
+    <div className="App">
+      <Profile item={person} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Profile/Person.js
+++ src/components/Profile/Person.js
@@ -1,36 +1,56 @@
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
 const Person = ({
   name,
   tag,
   location,
   avatar,
-  stats: { followers, views, likes },
+  stats,
+  followers,
+  likes,
+  views,
 }) => {
-  return;
-  <div className="profile">
-    <div className="description">
-      <img src={avatar} alt="Аватар пользователя" className="avatar" />
-      <p className="name">{name}</p>
-      <p className="tag">{tag}</p>
-      <p className="location">{location}</p>
-    </div>
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt="Аватар пользователя" className="avatar" />
+        <p className="name">{name}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
+      </div>
 
-    <ul className="stats">
-      <li>
-        <span className="label">Followers</span>
-        <span className="quantity">{followers}</span>
-      </li>
-      <li>
-        <span className="label">Views</span>
-        <span className="quantity">{views}</span>
-      </li>
-      <li>
-        <span className="label">Likes</span>
-        <span className="quantity">{likes}</span>
-      </li>
-    </ul>
-  </div>;
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">
+            {followers}>{stats}
+          </span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">
+            {views}>{stats}
+          </span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">
+            {likes}>{stats}
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
 };
+
+// Person.PropTypes = {
+//   avatar: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   tag: PropTypes.string.isRequired,
+//   location: PropTypes.string.isRequired,
+//   followers: PropTypes.number.isRequired,
+//   views: PropTypes.number.isRequired,
+//   likes: PropTypes.number.isRequired,
+// };
 
 export default Person;
